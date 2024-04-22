b511c9cd3f151c0e00e9d9e7e8711e4daaabccb3
tsv.titan@gmail.com <tsv.titan@gmail.com>
2024-04-22 09:31:47 +0300

    Fixed fileExists

--- file.go
+++ file.go
@@ -6,18 +6,18 @@ import (
 
 func FileExists(path string) bool {
 
-	_, err := os.Stat(path)
+	info, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		return false
 	}
-	return err == nil
+	return !info.IsDir()
 }
 
 func DirExists(path string) bool {
 
-	_, err := os.Stat(path)
+	info, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		return false
 	}
-	return err == nil
+	return info.IsDir()
 }
