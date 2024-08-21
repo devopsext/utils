0c61a460a13edd1ee02bced008cf349019c6f7d6
tsv.titan@gmail.com <tsv.titan@gmail.com>
2024-08-21 17:06:00 +0300

    Extended EnvGet

--- env.go
+++ env.go
@@ -13,14 +13,32 @@ func EnvGet(key string, defaultValue interface{}) interface{} {
 	}
 
 	switch defaultValue.(type) {
-	case int:
+	case int, int32:
 		r, err := strconv.Atoi(value)
 		if err != nil {
 			return defaultValue
 		}
 		return r
+	case int64:
+		r, err := strconv.Atoi(value)
+		if err != nil {
+			return defaultValue
+		}
+		return int64(r)
 	case bool:
 		r, err := strconv.ParseBool(value)
+		if err != nil {
+			return defaultValue
+		}
+		return r
+	case float32:
+		r, err := strconv.ParseFloat(value, 32)
+		if err != nil {
+			return defaultValue
+		}
+		return r
+	case float64:
+		r, err := strconv.ParseFloat(value, 64)
 		if err != nil {
 			return defaultValue
 		}
