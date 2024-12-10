58899f67cf9323f317913c1451e6b5cc698ca8d3
Vladislav Radin <vladislav.radin@exness.com>
2024-12-10 10:03:27 +0200

    add new http function

--- http.go
+++ http.go
@@ -5,6 +5,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"math"
 	"net"
 	"net/http"
 	"time"
@@ -73,8 +74,71 @@ func HttpRequestRawWithHeadersOutCode(client *http.Client, method, URL string, h
 	return b, resp.StatusCode, nil
 }
 
+func HttpRequestRawWithRetry(client *http.Client, method, URL string, headers map[string]string, raw []byte, maxRetries int, retryHeader string) (body []byte, code int, err error) {
+
+	var reader io.Reader
+	if raw != nil {
+		reader = bytes.NewReader(raw)
+	}
+
+	var b []byte
+
+	req, err := http.NewRequest(method, URL, reader)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	for k, v := range headers {
+		if IsEmpty(v) {
+			continue
+		}
+		req.Header.Set(k, v)
+	}
+
+	for attempt := 0; attempt < maxRetries; attempt++ {
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil, resp.StatusCode, err
+		}
+		defer resp.Body.Close()
+
+		if !IsEmpty(resp.Body) {
+			b, err = io.ReadAll(resp.Body)
+			if err != nil {
+				return nil, resp.StatusCode, err
+			}
+		}
+
+		if resp.StatusCode == http.StatusTooManyRequests {
+
+			retryAfter := resp.Header.Get(retryHeader)
+
+			if retryAfter != "" {
+				duration, err := time.ParseDuration(retryAfter)
+				if err == nil {
+					time.Sleep(duration)
+					continue
+				}
+			}
+			backoff := time.Duration(math.Pow(2, float64(attempt))) * time.Second
+			time.Sleep(backoff)
+		}
+		if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+			return b, resp.StatusCode, fmt.Errorf(resp.Status)
+		}
+		return b, resp.StatusCode, nil
+	}
+	return nil, 0, fmt.Errorf("max retries exceeded")
+}
+
 func HttpRequestRawWithHeaders(client *http.Client, method, URL string, headers map[string]string, raw []byte) ([]byte, error) {
 	b, _, err := HttpRequestRawWithHeadersOutCode(client, method, URL, headers, raw)
+	return b, err
+}
+
+func HttpRequestRawWithHeadersRetry(client *http.Client, method, URL string, headers map[string]string, raw []byte, maxRetries int, retryHeader string) ([]byte, error) {
+	b, _, err := HttpRequestRawWithRetry(client, method, URL, headers, raw, maxRetries, retryHeader)
 	return b, err
 }
 
@@ -82,10 +146,20 @@ func HttpPostRawWithHeaders(client *http.Client, URL string, headers map[string]
 	return HttpRequestRawWithHeaders(client, "POST", URL, headers, raw)
 }
 
+func HttpPostRawWithHeadersRetry(client *http.Client, URL string, headers map[string]string, raw []byte, maxRetries int, retryHeader string) ([]byte, error) {
+	return HttpRequestRawWithHeadersRetry(client, "POST", URL, headers, raw, maxRetries, retryHeader)
+}
+
 func HttpPostRaw(client *http.Client, URL, contentType string, authorization string, raw []byte) ([]byte, error) {
 
 	headers := httpContentTypeAndAuthorizationHeaders(contentType, authorization)
 	return HttpPostRawWithHeaders(client, URL, headers, raw)
+}
+
+func HttpPostRawRetry(client *http.Client, URL, contentType string, authorization string, raw []byte, maxRetries int, retryHeader string) ([]byte, error) {
+
+	headers := httpContentTypeAndAuthorizationHeaders(contentType, authorization)
+	return HttpPostRawWithHeadersRetry(client, URL, headers, raw, maxRetries, retryHeader)
 }
 
 func HttpPostRawWithHeadersOutCode(client *http.Client, URL string, headers map[string]string, raw []byte) (body []byte, code int, err error) {
@@ -122,9 +196,18 @@ func HttpGetRawWithHeaders(client *http.Client, URL string, headers map[string]s
 	return HttpRequestRawWithHeaders(client, "GET", URL, headers, nil)
 }
 
+func HttpGetRawWithHeadersRetry(client *http.Client, URL string, headers map[string]string, maxRetries int, retryHeader string) ([]byte, error) {
+	return HttpRequestRawWithHeadersRetry(client, "GET", URL, headers, nil, maxRetries, retryHeader)
+}
+
 func HttpGetRaw(client *http.Client, URL, contentType string, authorization string) ([]byte, error) {
 	headers := httpContentTypeAndAuthorizationHeaders(contentType, authorization)
 	return HttpGetRawWithHeaders(client, URL, headers)
+}
+
+func HttpGetRawRetry(client *http.Client, URL, contentType string, authorization string, maxRetries int, retryHeader string) ([]byte, error) {
+	headers := httpContentTypeAndAuthorizationHeaders(contentType, authorization)
+	return HttpGetRawWithHeadersRetry(client, URL, headers, maxRetries, retryHeader)
 }
 
 func HttpGetHeader(client *http.Client, URL string) (map[string][]string, error) {
